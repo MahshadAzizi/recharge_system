9c96c7d3c052ef2bcdef50aa70a99b222007a062
mahshad <azizi.mhshd@gmail.com>
2024-11-13 16:19:37 +0330

    feat: add credit request view

---
+++ management/api/views/credit_request.py
@@ -0,0 +1,20 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from management.api.serializers import CreditRequestSerializer
+
+
+class CreditIncreaseRequestView(APIView):
+    def post(self, request):
+        serializer = CreditRequestSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message": f"Credit increase request for {serializer.validated_data['amount']} submitted successfully."
+            }, status=status.HTTP_201_CREATED)
+
+        return Response({
+            "errors": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
