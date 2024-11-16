620d9b0a550f2b1625ac88d80c0c2a00c950ca4d
mahshad <azizi.mhshd@gmail.com>
2024-11-16 11:43:00 +0330

    fix: remove prints

--- management/api/views/approve_credit_request.py
+++ management/api/views/approve_credit_request.py
@@ -10,19 +10,14 @@ class AdminCreditApprovalView(APIView):
     Endpoint for admins to approve or reject credit requests.
     """
     def post(self, request):
-        print("Request data:", request.data)
-
         serializer = AdminCreditApprovalSerializer(data=request.data)
         if serializer.is_valid():
             try:
                 credit_request = serializer.save()
-                print("Approved:", credit_request)
                 return Response({
                     'request_id': credit_request.request_id,
                     'message': f'Credit request {credit_request.id} updated to {credit_request.status}.'
                 }, status=status.HTTP_200_OK)
             except ValidationError as e:
-                print("Validation Error:", e)
                 return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        print("Errors:", serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
