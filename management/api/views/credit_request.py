d3c058ef78e859d47cebf67dbd425319ff74c842
mahshad <azizi.mhshd@gmail.com>
2024-11-16 14:30:20 +0330

    fix: remove index from seller name and change view

--- management/api/views/credit_request.py
+++ management/api/views/credit_request.py
@@ -8,9 +8,11 @@ from management.api.serializers import CreditRequestSerializer
 class CreditIncreaseRequestView(APIView):
     def post(self, request):
         serializer = CreditRequestSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({
-            'message': f"Credit increase request for {serializer.validated_data['amount']} submitted successfully."
-        }, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'message': f"Credit increase request for {serializer.validated_data['amount']} submitted successfully."
+            }, status=status.HTTP_201_CREATED)
 
+        else:
+            return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
